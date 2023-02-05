6cc65b074e9c1b254c4725abebd93008c256c44d
fangkuaib <fangkuaib@163.com>
2023-02-05 14:21:09 +0800

    url路径参数

--- bookmanager04/book/urls.py
+++ bookmanager04/book/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from book.views import create_book
+from book.views import create_book, goods
 
 urlpatterns = (
     path('create_book/', create_book),
-
+    path('<cat_id>/<id>/', goods),
 )
--- bookmanager04/book/views.py
+++ bookmanager04/book/views.py
@@ -1,5 +1,17 @@
 from django.http import HttpResponse
+from book.models import BookInfo
 
 
 def create_book(request):
+    book = BookInfo.objects.create(
+        name='abc',
+        pub_date='2022-02-05',
+        readcount=1,
+        commentcount=2
+    )
+
     return HttpResponse("create sucess")
+
+
+def goods(request, cat_id, id):
+    return HttpResponse('sucess ')
