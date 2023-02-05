16a1974cc66b1a1f22161d91b45694a4d5ce6373
fangkuaib <fangkuaib@163.com>
2023-02-05 17:23:13 +0800

    method

--- bookmanager04/book/urls.py
+++ bookmanager04/book/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from book.views import create_book, goods, register, json_data
+from book.views import create_book, goods, register, json_data, method
 
 urlpatterns = (
     path('create_book/', create_book),
-    path('<cat_id>/<id>/', goods),
+    # <转换器名字：变量名>
+    # 转换器会对变量名进行正则的验证
+    path('<int:cat_id>/<int:id>/', goods),
     path('register/', register),
-    path('json_data/', json_data)
+    path('json_data/', json_data),
+    path('method/', method)
 )
