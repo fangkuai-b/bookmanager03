27eca238abfca4d6fc07bb69428a704b6a5d40af
fangkuaib <fangkuaib@163.com>
2023-02-05 15:37:18 +0800

    查询字符串和post form表单

--- bookmanager04/book/urls.py
+++ bookmanager04/book/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from book.views import create_book, goods
+from book.views import create_book, goods, register
 
 urlpatterns = (
     path('create_book/', create_book),
     path('<cat_id>/<id>/', goods),
+    path('register/', register)
 )
--- bookmanager04/book/views.py
+++ bookmanager04/book/views.py
@@ -14,4 +14,28 @@ def create_book(request):
 
 
 def goods(request, cat_id, id):
+    query_parmas = request.GET
+    print(
+        query_parmas)  # http://127.0.0.1:8000/101/10055/?order=desc  返回打印结果：<QueryDict: {'a': ['1', '3'], 'b': ['2']}>
+    a = query_parmas.get('a')
+    b = query_parmas['b']
+    a_list = query_parmas.getlist('a')
+    print(a)  # 3
+    print(b)  # 2
+    print(a_list)  # ['1', '3']
     return HttpResponse('sucess ')
+
+
+'''
+查询字符串
+http://ip:port/path/path/:?key1=value1&key2=value2
+url 以 ? 分割为2部分
+?之前的部分为请求路径
+?之后的部分为查询字符串，类似于字典的key=value形式，多个数据采用&符拼接
+'''
+
+
+def register(request):
+    data = request.POST
+    print(data)
+    return HttpResponse('OK')
