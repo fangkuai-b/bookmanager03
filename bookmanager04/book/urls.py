78cdb86e85d09c0a209df951360de909e1f070c9
fangkuaib <fangkuaib@163.com>
2023-02-05 15:59:42 +0800

    接受json数据

--- bookmanager04/book/urls.py
+++ bookmanager04/book/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from book.views import create_book, goods, register
+from book.views import create_book, goods, register, json_data
 
 urlpatterns = (
     path('create_book/', create_book),
     path('<cat_id>/<id>/', goods),
-    path('register/', register)
+    path('register/', register),
+    path('json_data/', json_data)
 )
--- bookmanager04/book/views.py
+++ bookmanager04/book/views.py
@@ -1,3 +1,5 @@
+import json
+
 from django.http import HttpResponse
 from book.models import BookInfo
 
@@ -37,5 +39,26 @@ url 以 ? 分割为2部分
 
 def register(request):
     data = request.POST
-    print(data)
+    print(data)  # <QueryDict: {'username': ['itcast'], 'password': ['123']}>
     return HttpResponse('OK')
+
+
+def json_data(request):
+    body = request.body
+    print(body)  # b'{\r\n    "a": 1,\r\n    "b": "2b2",\r\n    "c": "c"\r\n}'
+    body_str = body.decode()  # 可省略
+    print(body_str)
+    '''
+    {
+        "a": 1,
+        "b": "2b2",
+        "c": "c"
+    }
+    '''
+    print(type(body_str))  # <class 'str'>
+    req_data = json.loads(body_str)
+    print(req_data)  # {'a': 1, 'b': '2b2', 'c': 'c'}
+    print(req_data['a'])  # 1
+    print(req_data['b'])  # 2b2
+    print(req_data['c'])  # c
+    return HttpResponse('json')
